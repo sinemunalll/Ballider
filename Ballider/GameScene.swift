459bc1f29d20838cc701eb7e140e7fb2587f49ff
sinem <19sinemunal@gmail.com>
2022-10-19 21:37:22 +0300

    minör refactoring

--- Ballider/GameScene.swift
+++ Ballider/GameScene.swift
@@ -16,6 +16,7 @@ enum ColliderType : UInt32{
 
 class GameScene: SKScene,SKPhysicsContactDelegate {
     
+    static let LEVEL_MAX_POINT = 5
     
     let ball = Ball()
     let brick = Brick()
@@ -37,6 +38,9 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
     var progressCount = 0
     
     var lv1 = 10
+    
+    let storedHighScore = UserDefaults.standard.object(forKey: "highScore")
+    
     
   
     override func didMove(to view: SKView) {
@@ -73,15 +77,13 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
         ball.setup()
         ball.collider()// carpisma
         
-        self.storeHighScoreCheck()
+        self.checkStoreHighScore()
    
     }
     
-    func storeHighScoreCheck(){
+    func checkStoreHighScore(){
       
-        let storedHighScore = UserDefaults.standard.object(forKey: "highScore")
-    
-        if storedHighScore == nil {
+        if self.storedHighScore == nil {
             highScore = 0
         }
         
@@ -90,100 +92,63 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
         }
     }
     
+
+    
     func didBegin(_ contact: SKPhysicsContact) {
         self.nextLevel()
         gameStarted = true
          if contact.bodyA.collisionBitMask == ColliderType.Ball.rawValue || contact.bodyB.collisionBitMask == ColliderType.Ball.rawValue {
              
              ball.physicsBody?.applyImpulse(randomImpulse(dxPosition: -500, dyPosition: 500))
-             ball.physicsBody?.affectedByGravity = true
         
-             brick.setWidth(width: brick.getWidth() - 10)
-             brick.setHeight(height: brick.getHeight() - 10)
-           
-             score += 1
-             scoreLabel.changeString(score: score)
+             self.scoreIncreaseAndChangeScoreLabel()
              scoreLabel.run(.sequence([.scale(to: 1.3, duration: 0.1),.scale(to: 1.0, duration: 0.1)])) //effect scale
              
-             if score > highScore {
-                 highScore = score
-                 defaults.set(highScore, forKey: "highScore")
-             }
+             self.checkAndSetHighScore()
              
-            if self.progressCount <= 10 {
-                 self.progressBar.updateProgressBar(updateProgress: 1, updateDurationProgress: 0.1)
-                 self.progressCount += 1
+             if self.score <= GameScene.LEVEL_MAX_POINT {
+                self.updateProgressBar()
              }
          }
      }
     
-    func randomImpulse(dxPosition: CGFloat, dyPosition: CGFloat) -> CGVector {
-        let dx = (CGFloat.random(in: -(250)...250) - ball.position.x)
-        let dy = -((ball.position.y) - ball.getOriginalPositions().y)
-        let impulse = CGVector(dx: dx, dy: dy)
-        return impulse
+    func scoreIncreaseAndChangeScoreLabel(){
+        self.score += 1
+        scoreLabel.changeString(score: score)
     }
     
-    func touchDown(atPoint pos : CGPoint) {
-     
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-      
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-      
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {
-            let touchLocation = touch.location(in: self)
-            let touchNodes = nodes(at: touchLocation) 
-                if touchNodes.isEmpty == false {
-                    for node in touchNodes {
-                        if let sprite = node as? SKSpriteNode {
-                            if sprite == brick {
-                                brick.position = CGPoint(x:touchLocation.x, y:brick.position.y)
-                        }
-                            if sprite == button {
-                                self.loadGameScene()
-                            }
-                    }
-                }
-            }
+    public func checkAndSetHighScore(){
+        if self.score > self.highScore {
+            self.highScore = self.score
+            defaults.set(highScore, forKey: "highScore")
         }
     }
     
+    
+    public func updateProgressBar(){
+        
+        self.progressBar.updateProgressBar(updateProgress: 1, updateDurationProgress: 0.1)
+        self.progressCount += 1
+    }
+    
+    
+    func randomImpulse(dxPosition: CGFloat, dyPosition: CGFloat) -> CGVector {
+        let dx = (CGFloat.random(in: -(250)...250) - ball.position.x)
+        let dy = -((ball.position.y) - ball.getOriginalPositions().y)
+    
+        return CGVector(dx: dx, dy: dy)
+    }
+    
+  
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.touches(touches: touches)
+    }
+    
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let touch = touches.first {
-                    let touchLocation = touch.location(in: self)
-          
-                    let touchNodes = nodes(at: touchLocation) //dokunulan yerdeki node
-                           if touchNodes.isEmpty == false {
-                                 for node in touchNodes {
-                                     
-                                     if let sprite = node as? SKSpriteNode {
-                                         if sprite == brick {
-                                             brick.position = CGPoint(x:touchLocation.x, y:brick.position.y)
-                                         }
-                                         if sprite == button {
-                                             self.loadGameScene()
-                                         }
-                                     }
-                                 }
-                             }
-                         }
+        self.touches(touches: touches)
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-       
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-    
-    }
-    
+   
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
         if let ballPhysicsBody = ball.physicsBody {
@@ -216,6 +181,25 @@ class GameScene: SKScene,SKPhysicsContactDelegate {
         if score == lv1 {
             self.sceneDidLoad()
             self.loadGameScene()
+        }
+    }
+    
+    func touches(touches: Set<UITouch>){
+        if let touch = touches.first {
+            let touchLocation = touch.location(in: self)
+            let touchNodes = nodes(at: touchLocation)
+                if touchNodes.isEmpty == false {
+                    for node in touchNodes {
+                        if let sprite = node as? SKSpriteNode {
+                            if sprite == brick {
+                                brick.position = CGPoint(x:touchLocation.x, y:brick.position.y)
+                        }
+                            if sprite == button {
+                                self.loadGameScene()
+                            }
+                    }
+                }
+            }
         }
     }
        
